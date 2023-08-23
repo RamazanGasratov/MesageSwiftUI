e6db5849e6a251025810b88eee4a2d39fc36ea25
RamazanIOS <gasratov99@bk.ru>
2023-08-23 13:06:29 +0300

    Initial Commit

---
+++ MasegeSwiftUI/MasegeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasegeSwiftUIApp.swift
+//  MasegeSwiftUI
+//
+//  Created by Ramazan Gasratov on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MasegeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
